34138a637decd37b77459433acfd75e6c2f08390
noelfoster7 <150407433+noelfoster7@users.noreply.github.com>
2023-12-08 15:09:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 
-file_path = 'https://github.com/noelfoster7/Final_proj_app/blob/main/social_media_usage.csv'
-s = pd.read_csv(file_path)
+file_path = '/Users/noelfoster/Desktop/stream_folder/social_media_usage.csv'
+ss = pd.read_csv(file_path)
 
 def clean_sm(x):
     return np.where(x == 1, 1, 0)
